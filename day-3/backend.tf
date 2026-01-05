03606c30b5581cefdfa20f17d7e897ce4df8201a
Krrish166 <bijayapatra133@gmail.com>
2026-01-05 13:13:17 +0530

    day-3/backend.tf

--- day-3/backend.tf
+++ day-3/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "dsfgsdvdfffffdfdfd"  #here bucket is name of s3 bucket
     key    = "terraform.tfstate"  #here key is path inside the bucket == you can direct file name or path name
     region = "ap-south-1"
+    use_lockfile = true  #enables state locking to prevent concurrent operations
   }
 }
